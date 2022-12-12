db39d5b781ef0020d7d75ea0e00c9099c9bd9a10
carlyn.1 <carlyn.1@cse-cnc238812d.coeit.osu.edu>
2022-12-12 14:41:41 -0500

    Polished training script, saving mechanisms, generalizable

---
+++ test.py
@@ -0,0 +1,7 @@
+
+if __name__ == "__main__":
+    #! 1. Load args
+    #! 2. Load Model
+    #! 3. Load Data
+    #! 4. Run inference
+    #! 5. Plot/print results
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import random
 
+from argparse import ArgumentParser
+
 import torch
 import numpy as np
 
@@ -7,3 +9,18 @@ def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument("--seed", type=int, default=2022)
+    parser.add_argument("--hidden_channels", type=int, default=10)
+    parser.add_argument("--out_channels", type=int, default=10)
+    parser.add_argument("--epochs", type=int, default=50)
+    parser.add_argument("--train_samples", type=int, default=8000)
+    parser.add_argument("--val_samples", type=int, default=1000)
+    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--dset", type=str, choices=["asia", "earthquake"], default="asia")
+    parser.add_argument("--i_vars", type=str, nargs="+", default=[])
+    parser.add_argument("--i_probs", type=float, nargs="+", default=[])
+
+    return parser.parse_args()
